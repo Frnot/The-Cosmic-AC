21c7bd67553aac76b9902983e1478ee2fe2841f2
Frnot <frnot@frnot.com>
2020-12-19 15:37:34 -0600

    configured logging

--- main.py
+++ main.py
@@ -5,17 +5,37 @@ from dotenv import load_dotenv
 
 # Modules
 from utils import *
+from cmd_main import *
 
+# Configure root (global) logger
+root_logger = logging.getLogger('')
+root_logger.setLevel(logging.NOTSET)
+lso = logging.StreamHandler()
+lfo = logging.FileHandler('main.log')
+lso.setLevel(logging.WARNING)
+lfo.setLevel(logging.DEBUG)
+format = logging.Formatter('%(asctime)s:%(name)-12s:%(levelname)-8s - %(message)s')
+lso.setFormatter(format)
+lfo.setFormatter(format)
+root_logger.addHandler(lso)
+root_logger.addHandler(lfo)
+# configure local logger
 log = logging.getLogger(__name__)
+
+
+
+
 load_dotenv()
 BOT_TOKEN = os.getenv("TOKEN")
 
 class MyClient(discord.Client):
     async def on_ready(self):
-        log('Logged on as {0}!'.format(self.user), 3)
+        log.info('Logged on as {0}!'.format(self.user))
 
     async def on_message(self, message):
-        log('Message from {0.author}: {0.content}'.format(message), 3)
+        log.info('Message from {0.author}: {0.content}'.format(message))
 
-client = MyClient()
-client.run(BOT_TOKEN)
+#client = MyClient()
+#client.run(BOT_TOKEN)
+log.info("running test now")
+test()
