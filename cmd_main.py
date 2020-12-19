a2e6704bb14f1ba9dcf790a27276c22fa5426ade
Frnot <frnot@frnot.com>
2020-12-19 15:37:34 -0600

    configured logging

---
+++ cmd_main.py
@@ -0,0 +1,5 @@
+import logging
+log = logging.getLogger(__name__)
+
+def test():
+    log.warning('testing')
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
    name='CosmicAC',
-   version='0.0.1',
+   version='0.0.2',
    description='A discord.py discord bot',
    install_requires=['discord.py', 'python-dotenv'],
 )
