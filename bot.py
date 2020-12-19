3c96317ceb578ee1b94c4a97290244325552d2c1
Frnot <frnot@frnot.com>
2020-12-18 23:31:23 -0600

    added utils.py and started logging system

--- bot.py
+++ bot.py
@@ -1,18 +1,20 @@
-import discord
 import os
+import discord
 from dotenv import load_dotenv
 
+# Modules
+from utils import *
+log = Utils.log
+
 load_dotenv()
-
 BOT_TOKEN = os.getenv("TOKEN")
-
 
 class MyClient(discord.Client):
     async def on_ready(self):
-        print('Logged on as {0}!'.format(self.user))
+        log('Logged on as {0}!'.format(self.user), 3)
 
     async def on_message(self, message):
-        print('Message from {0.author}: {0.content}'.format(message))
+        log('Message from {0.author}: {0.content}'.format(message), 3)
 
 client = MyClient()
 client.run(BOT_TOKEN)
