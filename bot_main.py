010082ab1a39f5cc6350e717dedbcb32b707afe5
Frnot <frnot@frnot.com>
2020-12-19 23:08:39 -0600

    refactored main bot code

--- bot_main.py
+++ bot_main.py
@@ -5,20 +5,24 @@ from dotenv import load_dotenv
 
 log = logging.getLogger(__name__)
 
+# Enable intents
+intents = discord.Intents.default()
+intents.members = True
 
-class MyClient(discord.Client):
-    async def on_ready(self):
-        log.info('Logged on as {0}!'.format(self.user))
+client = discord.Client
 
-    async def on_message(self, message):
-        log.info('Message from {0.author}: {0.content}'.format(message))
 
-    #async def on_message(self, message):
-        
+@client.event
+async def on_ready():
+    log.info('Logged on as {0}!'.format(client.user))
+
+
+@client.event
+async def on_message(self, message):
+    log.info('Message from {0.author}: {0.content}'.format(message))
 
 
 def run_bot():
     load_dotenv()
     BOT_TOKEN = os.getenv("TOKEN")
-    client = MyClient()
     client.run(BOT_TOKEN)
