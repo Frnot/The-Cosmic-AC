4792f3c29e7094557f0c4b52217e1af0fce481a1
Frnot <frnot@frnot.com>
2021-04-02 12:56:23 -0500

    trimmed owner ids

--- utils.py
+++ utils.py
@@ -3,6 +3,6 @@ import logging
 log = logging.getLogger(__name__)
 
 #class Utils:
-priv_user_ids = [175786263201185792, 483039138178662400]
+priv_user_ids = [175786263201185792]
 async def is_owner(ctx):
     return ctx.author.id in priv_user_ids
