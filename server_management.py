037dd8c9d3fc9cd9a777268955c71e062c1c48db
Frnot <frnot@frnot.com>
2021-04-17 12:55:26 -0500

    added default role automation

--- server_management.py
+++ server_management.py
@@ -10,11 +10,19 @@ class Cog(commands.Cog, name='Server Management'):
         self.bot = bot
         log.info(f"Registered Cog: {self.qualified_name}")
 
-    # Event Listeners
-    # Give every new member a role
+    
+    ## Give every new member a role
     @commands.Cog.listener()
     async def on_member_join(self, member):
         guild = member.guild
         newrole = guild.get_role(833019117992542248)
         await member.add_roles(newrole)
         log.info(f"{member.display_name} has been assigned the role `{newrole.name}`.")
+
+    @commands.command()
+    async def roleup(self, ctx):
+        newrole = ctx.guild.get_role(833019117992542248)
+        for member in ctx.guild.members:
+            if newrole not in member.roles:
+                await member.add_roles(newrole)
+                log.info(f"{member.display_name} has been assigned the role `{newrole.name}`.")
