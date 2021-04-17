b2ea153a06003887c50fe15f5f138e0021a7d331
Frnot <frnot@frnot.com>
2021-04-17 13:00:29 -0500

    version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
    name='CosmicAC',
-   version='0.3.1',
+   version='0.3.2',
    description='A discord.py discord bot',
    url="https://github.com/Frnot/The-Cosmic-AC",
    install_requires=['discord.py', 'python-dotenv'],
