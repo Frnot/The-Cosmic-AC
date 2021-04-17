9a34206042b531c786f82a264d09355d5b51f390
Frnot <frnot@frnot.com>
2021-04-17 12:11:04 -0500

    added listroles command

--- cmd_main.py
+++ cmd_main.py
@@ -10,15 +10,24 @@ class Cog(commands.Cog, name='General commands'):
         self.bot = bot
         log.info(f"Registered Cog: {self.qualified_name}")
 
+    # debug ping
     @commands.command()
     async def i(self, ctx):
         await ctx.send(".")
 
     @commands.command()
+    async def listroles(celf, ctx):
+        role_list = ctx.guild.roles()
+        for role in role_list:
+            await ctx.send(f"Name: {role.name} - ID: {role.id}")
+
+    # wipe a chat the old fashioned way
+    @commands.command()
     async def nukechat(self, ctx):
         log.info(f"nuking channel: {ctx.channel} in guild: {ctx.guild}")
         await ctx.channel.purge()
 
+    # leave server
     @commands.command()
     @commands.check(utils.is_owner)
     async def leave(self, ctx):
