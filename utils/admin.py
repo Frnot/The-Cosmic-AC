0dda8cd39c8e197db9e6be30ccc9acd6a014a190
Frnot <frnot@frnot.com>
2021-05-31 19:10:30 -0500

    utilize bot built-in is_owner() func

--- utils/admin.py
+++ utils/admin.py
@@ -2,9 +2,9 @@ import logging
 log = logging.getLogger(__name__)
 
 
-owner_ids = [175786263201185792]
 async def is_owner(ctx):
-    return ctx.author.id in owner_ids
+    return await ctx.bot.is_owner(ctx.author) 
+
 
 async def is_server_owner(ctx):
     return ctx.author == ctx.guild.owner
