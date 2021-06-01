144af5bf286324ccadb126989fa6c37a87e2a49c
Frnot <frnot@frnot.com>
2021-05-31 19:10:30 -0500

    utilize bot built-in is_owner() func

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
    name='CosmicAC',
-   version='0.7.14',
+   version='0.7.15',
    description='A discord.py discord bot',
    url="https://github.com/Frnot/The-Cosmic-AC",
    install_requires=[
