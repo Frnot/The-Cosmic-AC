f310f5cb7dd001f7f8a0beca88232da116fbbe90
Frnot <frnot@frnot.com>
2021-06-03 02:59:05 -0500

    add custom per-guild command prefix

--- db_cache.py
+++ db_cache.py
@@ -6,21 +6,21 @@ import db
 import logging
 log = logging.getLogger(__name__)
 
-class DB_Cache:
+class DBCache:
     def __init__(self, table_name, key_name, value_name):
         self.cache_dict = {}
-        self.table = table_name
+        self.table_name = table_name
         self.key_name = key_name
         self.value_name = value_name
 
 
 
-    async def add(self, key, value):
+    async def add_or_update(self, key, value):
         if self.cache_dict.get(key) != value:
             if self.cache_dict.get(key) is None:
-                log.debug(f"Adding value for {key} in {self.table}")
+                log.debug(f"Adding value for {key} in {self.table_name}")
             else:
-                log.debug(f"Changing {key} in {self.table}")
+                log.debug(f"Changing {key} in {self.table_name}")
             self.cache_dict[key] = value
         
         await self.flush_cache_item_to_db(key)
@@ -38,13 +38,13 @@ class DB_Cache:
 
     async def get(self, key):
         if self.cache_dict.get(key) is None:
-            cache_item = await db.select(self.value_name, self.table, self.key_name, key)
+            cache_item = await db.select(self.value_name, self.table_name, self.key_name, key)
 
             if cache_item is None:
-                log.debug(f"Count not find {self.value_name} in table {self.table} for {self.key_name} = {key}")
+                log.debug(f"Could not find {self.value_name} in table {self.table_name} for {self.key_name} = {key}")
                 return None
             else:
-                log.debug(f"Found {self.value_name} in table {self.table} for {self.key_name} = {key}. Loading into cache")
+                log.debug(f"Found {self.value_name} in table {self.table_name} for {self.key_name} = {key}. Loading into cache")
                 self.cache_dict[key] = cache_item
         
         return self.cache_dict.get(key)
@@ -52,7 +52,7 @@ class DB_Cache:
 
     async def flush_cache_item_to_db(self, key):
         cache_item = self.cache_dict.get(key)
-        db_item = await db.select(self.value_name, self.table, self.key_name, key)
+        db_item = await db.select(self.value_name, self.table_name, self.key_name, key)
 
         if cache_item == db_item:
             log.debug(f"Cache and DB are already in sync for key '{key}' in table '{self.table_name}'")
---
+++ model/prefix.py
@@ -0,0 +1,26 @@
+import db_cache
+import asyncio
+import logging
+log = logging.getLogger(__name__)
+
+prefixes = db_cache.DBCache("cmd_prefix", "guild_id", "prefix")
+default_prefix = "./"
+
+
+
+async def get(guild_id):
+    prefix = await prefixes.get(guild_id)
+    if prefix is None:
+        return default_prefix
+    else:
+        return prefix
+
+
+async def add_or_update(guild_id, new_prefix):
+    await prefixes.add_or_update(guild_id, new_prefix)
+
+
+async def remove(guild_id):
+    prefix = await prefixes.get(guild_id)
+    if prefix is not None:
+        await prefixes.remove(guild_id)
