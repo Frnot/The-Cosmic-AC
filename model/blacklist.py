ef92af186bc20d242590924707f046b930830943
Frnot <frnot@frnot.com>
2021-06-04 14:35:52 -0500

    word on updating cache

---
+++ model/blacklist.py
@@ -0,0 +1,65 @@
+import db_cache
+import logging
+log = logging.getLogger(__name__)
+
+blacklists = db_cache.DBCache("blacklist", "guild_id", "blacklist_set")
+
+
+
+async def add_word(guild_id, word):
+    word_set = await blacklists.get(guild_id)
+    if word_set is None:
+        word_set = set()
+
+    log.debug(f"Adding word '{word}' to blacklist for guild id: {guild_id}")
+    word_set.add(word)
+    await blacklists.add_or_update(guild_id, word_set)
+
+
+
+async def remove_word(guild_id, word):
+    word_set = await blacklists.get(guild_id)
+    if word_set is None:
+            log.debug(f"Cannot remove {word} from blacklist for guild id: {guild_id}")
+    else:
+        if word in word_set:
+            log.debug(f"Removed word '{word}' from blacklist for guild id: {guild_id}")
+            word_set.remove(word)
+            await blacklists.add_or_update(guild_id, word_set)
+            return True
+        else:
+            log.debug(f"Word '{word}' does not exist in the blacklist for guild id: {guild_id}")
+            return False
+
+
+
+async def get(guild_id):
+    return await blacklists.get(guild_id)
+
+
+
+async def clear_blacklist(guild_id):
+    word_set = await blacklists.get(guild_id)
+    if word_set is None:
+        return False
+    else:
+        word_set.clear()
+        await blacklists.add_or_update(guild_id, word_set)
+        return True
+
+
+
+async def sync_blacklists(bot):
+    member_guild_ids = set([guild.id for guild in bot.guilds])
+    # .keys no bueno
+    blacklist_guild_ids = blacklists.keys()
+
+    log.debug("Pruning orphaned blacklists from cache")
+    for blacklist_guild_id in blacklist_guild_ids:
+        if blacklist_guild_id not in member_guild_ids:
+            await blacklists.remove(blacklist_guild_id)
+
+    log.debug("Loading blacklists for member guilds into cache")
+    for member_guild_id in member_guild_ids:
+        if await blacklists.get(member_guild_id) is None:
+            await blacklists.add_or_update(member_guild_id, set())
